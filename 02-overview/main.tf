3722e2837696c61083055cc586b0cdf6dddf4394
alditam <92889236+alditam@users.noreply.github.com>
2025-07-03 20:38:32 +0200

    main.tf updated

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -9,10 +9,10 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-central-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-092ff8e60e2d51e19" # Ubuntu 20.04 LTS // eu-west-1
+resource "aws_instance" "test" {
+  ami           = "ami-0229b8f55e5178b65" # Ubuntu 20.04 LTS // eu-west-1
   instance_type = "t2.micro"
 }
