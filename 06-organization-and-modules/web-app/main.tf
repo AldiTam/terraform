61d310221c9fad8746e1e864beb31308270dc8b1
alditam <92889236+alditam@users.noreply.github.com>
2025-07-17 09:33:52 +0200

    eu-central-1 updated

--- 06-organization-and-modules/web-app/main.tf
+++ 06-organization-and-modules/web-app/main.tf
@@ -2,9 +2,9 @@ terraform {
   # Assumes s3 bucket and dynamo DB table already set up
   # See /code/03-basics/aws-backend
   backend "s3" {
-    bucket         = "devops-directive-tf-state"
+    bucket         = "aldi-tf-state"
     key            = "06-organization-and-modules/web-app/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-central-1"
     dynamodb_table = "terraform-state-locking"
     encrypt        = true
   }
@@ -18,7 +18,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-central-1"
 }
 
 variable "db_pass_1" {
@@ -38,7 +38,7 @@ module "web_app_1" {
 
   # Input Variables
   bucket_prefix    = "web-app-1-data"
-  domain           = "devopsdeployed.com"
+  domain           = "devopsduckling.duckdns.org"
   app_name         = "web-app-1"
   environment_name = "production"
   instance_type    = "t2.micro"
@@ -53,7 +53,7 @@ module "web_app_2" {
 
   # Input Variables
   bucket_prefix    = "web-app-2-data"
-  domain           = "anotherdevopsdeployed.com"
+  domain           = "devopsquack.duckdns.org"
   app_name         = "web-app-2"
   environment_name = "production"
   instance_type    = "t2.micro"
